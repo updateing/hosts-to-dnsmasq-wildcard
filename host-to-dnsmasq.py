4c4c375fd647c219a53a637a7fca9eac58cd768f
Hamster Tian <haotia@gmail.com>
2015-07-13 12:05:17 +0800

    Use IP address as key, user-friendly comments, param check
    Previously we use truncated domain names as key, which may lead to
        192.168.1.2 x.aaa.com
        192.168.2.2 y.aaa.com
    being converted to
        address=/aaa.com/192.168.2.2
    
    This is obviously wrong. This commit will change it to
        address=/x.aaa.com/192.168.1.2
        address=/y.aaa.com/192.168.2.2
    
    Note that
        192.168.3.2 cdn1.aaa.com
        192.168.3.2 cdn2.aaa.com
    will be converted to
        address=/aaa.com/192.168.3.2
    If www.aaa.com uses different IP from its CDN,
    this site will be unusable. Don't forget to check output carefully!
    
    Other improvements:
     * Shows built-in README when parameters are missing
     * Shows error on IO errors: file not found, no permission etc
     * Make logic, comments, names clearer

--- host-to-dnsmasq.py
+++ host-to-dnsmasq.py
@@ -3,16 +3,22 @@
     Hosts to DNSMasq configuration converter
 
     Converts entries in a hosts file to dnsmasq conf,
-    taking wildcard into consideration. All sub-domains
-    are combined into one.
+    taking wildcard into consideration.
+    
+    Only truncate domain name when:
+        - One IP address has more than one sub-domains resolved to.
+    
+    USAGE: host-to-dnsmasq.py HostFile DnsmasqConfigFile
+        If DnsmasqConfigFile does not exist, it will be created.
+        If it exists, it will be OVERWRITTEN.
 '''
 
 import sys
 import string
+import errno
 
-# TODO: use address to collect domains - that makes more sense
-def findTopDomain(domain_name):
-    # Common country codes used in Google hosts
+def getTruncatedName(domainName):
+    # Common country codes
     countryName = ["cn", "sg", "jp", "hk", "uk", "ca", "ru", "kr", "gr", "bg", "cy", \
         "fi", "co", "au", "vi", "mn", "mm", "bn", "bz", "py", "ai", "ph", "pk", "pe", \
         "pg", "ar", "tj", "na", "ng", "nf", "fj", "np", "ni", "qa", "jm", "cu", "kw", \
@@ -22,20 +28,16 @@ def findTopDomain(domain_name):
         "zm", "za", "zw", "ve", "in", "il", "id", "ao", "uz", "ck", "cr", "bw", "tz"]
     topDomains = ["com", "org", "net", "co"]
 
-    domainElem = domain_name.split(".")
-    if len(domainElem) < 4 and len(domainElem[0]) < 7:
-        # enough to filter out "cdn-www.xda-developers.com" and "r12-xxxxxxx.googlevideo.com"
-        return domain_name
-    # TODO make these become parameters (minimum parts to be truncated and max length)
-
+    domainElem = domainName.split(".")
     if domainElem[-1] not in countryName: # *.com
         return string.join(domainElem[-2:], ".")
     else:
-        if domainElem[-2] not in topDomains: # *.cn
-            return string.join(domainElem[-2:], ".")
-        else: # *.com.cn, *.co.uk
-            return string.join(domainElem[-3:], ".")
+        if domainElem[-2] not in topDomains: # *.jp
+           return string.join(domainElem[-2:], ".")
+        else: # *.co.uk
+           return string.join(domainElem[-3:], ".")
 
+# addressMap: (address, set:[domain1, domain2, ..])
 def processLine(line, address_map):
     line = line.strip();
     if line.startswith("#") or line == "":
@@ -43,23 +45,44 @@ def processLine(line, address_map):
     else:
         lineElements = line.split()
         if len(lineElements) < 2:
-            print "Malformed host line \"%s\", elements < 2" % line
-            exit(1);
-        topDomain = findTopDomain(lineElements[1])
-        address_map[topDomain] = lineElements[0]
+            print "Malformed hosts line \"%s\", elements < 2" % line
+            exit(errno.EINVAL);
+        ip = lineElements[0]
+        domain = lineElements[1]
+        # Ensure every IP appears in conf
+        if ip not in address_map.keys():
+            # first appearance of this ip
+            # Do not truncate on first appearance,
+            # in order to keep signle IP's domain complete
+            address_map[ip] = set()
+            address_map[ip].add(domain)
+        else:
+            # ip appeared twice+, need to truncate
+            if len(address_map[ip]) == 1:
+                temp_domain = address_map[ip].pop()
+                address_map[ip].add(getTruncatedName(temp_domain))
+            address_map[ip].add(getTruncatedName(domain))
 
 def writeDnsmasqConf(confFile, addressMap):
     lines = []
-    for (key, value) in addressMap.items(): # TODO why?items()
-        lines.append("address=/%s/%s" % (key, value) + "\n")
+    for (ip, domains) in addressMap.items(): # TODO why?items()
+        for domain in domains:
+            lines.append("address=/%s/%s\n" % (domain, ip))
     confFile.writelines(lines)
 
 # Main program
-hostFile = open(sys.argv[1], "r")
-masqFile = open(sys.argv[2], "w")
+try:
+    hostFile = open(sys.argv[1], "r")
+    masqFile = open(sys.argv[2], "w")
+except IOError, e:
+    print "ERROR: IOError, ", e
+    exit(errno.EIO)
+except IndexError,e:
+    print __doc__
+    print "ERROR: Missing parameters. See Usage above."
+    exit(errno.EINVAL)
 lines = hostFile.readlines()
 addressMap = dict()
 for line in lines:
     processLine(line, addressMap)
 writeDnsmasqConf(masqFile, addressMap)
-
